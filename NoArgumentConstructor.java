e2ccfb9f1753bdb321bdb0f4c52722a193d03154
hemagowda5 <hemagowdaa27@gmail.com>
2026-06-27 16:48:42 +0530

    Add Student class with no-argument constructor

---
+++ NoArgumentConstructor.java
@@ -0,0 +1,11 @@
+class Student {
+    Student() {
+        System.out.println("No-Argument Constructor Called");
+    }
+}
+
+public class NoArgumentConstructor {
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
