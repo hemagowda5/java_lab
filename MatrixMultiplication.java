3dcc1e141b1eafe4a91458b69b56d0868b07dd3a
hemagowda5 <hemagowdaa27@gmail.com>
2026-06-28 12:39:43 +0530

    Implement matrix multiplication in Java

---
+++ MatrixMultiplication.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[2][2];
+        int[][] b = new int[2][2];
+        int[][] result = new int[2][2];
+
+        System.out.println("Enter First Matrix:");
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                a[i][j] = sc.nextInt();
+
+        System.out.println("Enter Second Matrix:");
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                b[i][j] = sc.nextInt();
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                result[i][j] = 0;
+
+                for (int k = 0; k < 2; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        System.out.println("Result Matrix:");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
